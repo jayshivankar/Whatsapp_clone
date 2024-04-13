60fe8359620fd7e25a9f6c5a7411756c12f41021
Jay <jp6631@srmist.edu.in>
2024-04-14 01:52:17 +0530

    Building Signup,Intro Screen

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+l
+<app/src/main/java/com/example/whatsapp_clone/MainActivity.kt,3/d/3d139f74bcabcedde65c4e269892ae9b6db9da85
+o
+?app/src/main/java/com/example/whatsapp_clone/Signup_activity.kt,1/8/18dfc7c2b5cb1961d1f5981cb7cf4835ee4bd388
+P
+ app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
+D
+app/build.gradle.kts,d/2/d2a4383452aad84a18429a5844d31330a012a9e6
+@
+build.gradle.kts,d/b/dbcff70658daf80b53ce624f6adcaa529df5ed8d
--- app/src/main/java/com/example/whatsapp_clone/MainActivity.kt
+++ app/src/main/java/com/example/whatsapp_clone/MainActivity.kt
@@ -15,32 +15,18 @@ import com.example.whatsapp_clone.ui.theme.Whatsapp_CloneTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        setContent(){
             Whatsapp_CloneTheme {
-                // A surface container using the 'background' color from the theme
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Whatsapp_CloneTheme {
-        Greeting("Android")
-    }
-}
+
