0665942d58c460122bb1e36737381287443398bd
Jay <jp6631@srmist.edu.in>
2024-04-15 21:55:08 +0530

    adding dependencies in gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+
 }
 
 android {
@@ -37,7 +38,7 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
-        compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.1"
@@ -61,8 +62,26 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    //Dagger Hilt
+    implementation ("com.google.dagger: hilt-android:2.44.2")
+
+    implementation ("androidx. fragment: fragment-ktx: 1.5.7")
+// Kotlin Extensions and Coroutines support for Room
+    implementation ("androidx.room: room-runtime: 2.5.1")
+// Coroutines
+    implementation ("org.jetbrains.kotlinx: kotlinx-coroutines-core:1.6.4")
+    implementation ("org.jetbrains.kotlinx: kotlinx-coroutines-android:1.6.4")
+// Retrofit
+    implementation ("com.squareup.retrofit2: retrofit: 2.9.0")
+    implementation ("com.squareup.retrofit2: converter-moshi:2.9.0")
+    implementation ("com.squareup.okhttp3: okhttp:5.0.0-alpha.3")
+    implementation ("com.squareup.okhttp3: logging-interceptor: 5.0.0-alpha.3")
+    implementation ("com.google.code.gson: gson:2.9.0")
+
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.google.firebase:firebase-auth-ktx:22.3.1")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.11.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
